c8dd702157c0ec487e1fcc36ffe1658f836f4648
Mike <mjeanjacques90@gmail.com>
2019-08-10 21:09:06 -0400

    Updates

---
+++ precache-manifest.2e6a3d12593a7fb7c26f8f167e252c8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6ba07bff9175ad8d9458a82af2ad7d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "75052e88926e5b12f618",
+    "url": "/clicky-game/static/css/main.9bc7f309.chunk.css"
+  },
+  {
+    "revision": "312e0fcd832fe92820bb",
+    "url": "/clicky-game/static/js/2.9452b0d2.chunk.js"
+  },
+  {
+    "revision": "75052e88926e5b12f618",
+    "url": "/clicky-game/static/js/main.93420692.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
